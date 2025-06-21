9c208a98dca8d75142b45c7fb17c7b9f4990cd28
Igor Alexeenko <164557400+iamo0@users.noreply.github.com>
2025-06-21 05:44:00 +0300

    Update cart-reducer.test.ts

--- src/reducers/cart-reducer.test.ts
+++ src/reducers/cart-reducer.test.ts
@@ -1,8 +1,18 @@
 import cartReducer, { CartReducerActionType } from "./cart-reducer";
 import { ProductInCart } from "@/types/product-in-cart";
 
+// NB! То, что тесты проверяют в первую очередь именно то, что возвращается
+// новый инстанс это не ошибка, это прямо критикал. Если бы функциональность
+// была сложнее, чем просто добавить, я бы делал через TDD и тестов было бы
+// больше, но сейчас через TLD я проверил критический путь. Критический он 
+// потому, что редьюсерам важно, чтобы после экшена вернулся новый инстанс
+
 describe("Cart reducer works correctly", function () {
   describe("Cart reducer adds products correctly", function () {
+    // ...Но вообще я выбирал между поведением корзины, которое есть сейчас
+    // и возможностью добавлять уже добавленный товар в корзину. Тогда на этот
+    // экшен было бы больше тестов, ну и работал бы он по другому, потому что он
+    // скорее всего бы суммировал quanity
     it("When called with adding action reducer returns new instance of state object", function () {
       const initialState = [] as ProductInCart[];
       expect(cartReducer(initialState, {
